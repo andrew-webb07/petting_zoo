b0d470f33ce96b47623585b81233a6531d77a39c
Andrew Webb <webba@ihopu.org>
2021-07-14 15:44:23 -0500

    added feed and __str__ methods to classes

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from walking import Camel, Cat, Dog, Goat, Horse
 from swimming import Catfish, Dolphin, Goldfish, Shark, Whale
 from slithering import Leech, Lizard, Salamander, Snake, Worm
 
-roberto = Camel("Roberto", "alpaca", "midday")
+roberto = Camel("Roberto", "alpaca", "midday", "grain")
 
-print(roberto.shift)
+print(roberto)
