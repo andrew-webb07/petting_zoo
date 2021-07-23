a41e4b9d8b7d39cd94796e87d52514d6181e2e11
Andrew Webb <webba@ihopu.org>
2021-07-23 11:35:35 -0500

    added chip property and getters to attractions

--- attractions/attractions.py
+++ attractions/attractions.py
@@ -10,6 +10,10 @@ class PettingZoo:
     def add_animal(self, animal):
         self.animals.append(animal)
 
+    @property
+    def last_critter_added(self):
+        return self.animals[-1]
+
 class SnakePit:
     def __init__(self, name, description):
         self.attraction_name = name
@@ -19,6 +23,10 @@ class SnakePit:
     def add_animal(self, animal):
         self.animals.append(animal)
 
+    @property
+    def last_critter_added(self):
+        return self.animals[-1]
+
 class Wetlands:
     def __init__(self, name, description):
         self.attraction_name = name
@@ -27,3 +35,7 @@ class Wetlands:
 
     def add_animal(self, animal):
         self.animals.append(animal)
+
+    @property
+    def last_critter_added(self):
+        return self.animals[-1]
