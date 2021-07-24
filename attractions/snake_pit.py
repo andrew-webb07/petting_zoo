0f87d029fd8f1ab7dc32eeb1b2fee91a1f0150b9
Andrew Webb <webba@ihopu.org>
2021-07-24 16:08:34 -0500

    added restrictions to add animal function

--- attractions/snake_pit.py
+++ attractions/snake_pit.py
@@ -1,11 +1,17 @@
 from attractions import Attraction
+from movements import Slithering
 
 class SnakePit(Attraction):
     def __init__(self, name, description):
         super().__init__(name, description)
 
-    def add_animal(self, animal):
-        self.animals.append(animal)
+    def add_animal_pythonic(self, animal):
+        try:
+            if animal.slither_speed > -1:
+                self.animals.append(animal)
+                print(f"{animal} now lives in {self.attraction_name}")
+        except AttributeError as ex:
+            print(f'{animal} doesn\'t like to be with snakes, so please do not put it in the {self.attraction_name} attraction.')
 
     @property
     def last_critter_added(self):
